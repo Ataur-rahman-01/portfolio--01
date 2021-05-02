e9f0dd15909a1bcfd6ba684b240c7cb731d2fda4
Ataur-rahman-01 <iamataur0911@gmail.com>
2021-05-02 21:49:00 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Ataur Rahman</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year}</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Ataur-rahman-01"
+                style={{ color: "white" }}
+              >
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/ataur-rahman-ab5425211/"
+                style={{ color: "white" }}
+              >
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/_md_ataur_rahman/"
+                style={{ color: "white" }}
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
